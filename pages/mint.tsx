a5da93bb8b0fe4270fde0b7f3f7ce2023aceb2af
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-04-11 17:15:33 +0300

    added mint and burn buttons functionality

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,90 +1,185 @@
-import * as utils from "lib/minting-utils"
-import { useCallback, useMemo, useState } from "react"
-import { useCardano, utility } from "use-cardano"
+// import * as utils from "lib/minting-utils";
+import * as utils from "lib/gcoinmint";
+import { useCallback, useMemo, useState, useEffect } from "react";
+import { useCardano, utility } from "use-cardano";
 
-import { Inter } from "@next/font/google"
-
-const inter = Inter({ subsets: ["latin"] })
+import { Inter } from "@next/font/google";
+// import { getStatus } from "../lib/minting-utils";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Mint() {
-  const { lucid, account, showToaster, hideToaster } = useCardano()
-
-  const [name, setName] = useState("")
+  const { lucid, account, showToaster, hideToaster } = useCardano();
+  const name: string = "GCOINFT";
+  const [mintAmount, setMintAmount] = useState(0);
+  const [burnAmount, setBurnAmount] = useState(0);
+  const [datum, setDatum] = useState<{ state: boolean; exchangeRate: bigint }>({
+    state: false,
+    exchangeRate: BigInt(0),
+  });
+  const [totalAmount, setTotalAmount] = useState(0n);
+  const init = async () => {
+    if (!lucid || !account?.address || !setMintAmount) return;
+    utils.getDatumValue(lucid, name).then((r) => setDatum({ ...r }));
+    utils.getAddressAda(lucid, name).then((r) => setTotalAmount(r));
+  };
+  useEffect(() => {
+    init();
+  }, [lucid, account?.address, mintAmount, burnAmount]);
 
   const mintNFT = useCallback(async () => {
     try {
-      if (!lucid || !account?.address || !name) return
+      if (!lucid || !account?.address || !setMintAmount) return;
 
-      const nftTx = await utils.mintNFT({ lucid, address: account.address, name })
+      const nftTx = await utils.mintGcoin(lucid, mintAmount, name);
 
-      showToaster("Minted NFT", `Transaction: ${nftTx}`)
+      showToaster("Minted NFT", `Transaction: ${nftTx}`);
     } catch (e) {
-      if (utility.isError(e)) showToaster("Could not mint NFT", e.message)
-      else if (typeof e === "string") showToaster("Could not mint NFT", e)
+      if (utility.isError(e)) showToaster("Could not mint NFT", e.message);
+      else if (typeof e === "string") showToaster("Could not mint NFT", e);
     }
-  }, [lucid, account?.address, showToaster, name])
+  }, [lucid, account?.address, showToaster, mintAmount]);
 
   const burnNFT = useCallback(async () => {
     try {
-      if (!lucid || !account?.address || !name) return
+      if (!lucid || !account?.address || !burnAmount) return;
 
-      const nftTx = await utils.burnNFT({ lucid, address: account?.address, name })
-
-      showToaster("Burned NFT", `Transaction: ${nftTx}`)
+      const nftTx = await utils.burnGcoin(lucid, burnAmount, name);
+      showToaster("Minted NFT", `Transaction: ${nftTx}`);
     } catch (e) {
-      if (utility.isError(e)) showToaster("Could not burn NFT", e.message)
-      else if (typeof e === "string") showToaster("Could not burn NFT", e)
+      if (utility.isError(e)) showToaster("Could not mint NFT", e.message);
+      else if (typeof e === "string") showToaster("Could not mint NFT", e);
     }
-  }, [lucid, account?.address, showToaster, name])
+  }, [lucid, account?.address, showToaster, burnAmount]);
 
-  const canMint = useMemo(() => lucid && account?.address && name, [lucid, account?.address, name])
+  const canMint = useMemo(
+    () => lucid && account?.address && mintAmount,
+    [lucid, account?.address, mintAmount]
+  );
 
+  const canBurn = useMemo(
+    () => lucid && account?.address && burnAmount,
+    [lucid, account?.address, burnAmount]
+  );
+  // max-w-4xl m-auto
   return (
-    <div className="text-center max-w-4xl m-auto text-gray-900 dark:text-gray-100">
+    <div className="text-center text-gray-900 dark:text-gray-100">
+      <div className="absolute right-40 rounded-xl bg-sky-800 py-2  hover:opacity-80 duration-200 disabled:opacity-30 w-40 text-white">
+        Total reserve of Ada ≈{Number(totalAmount) / 1000000} ₳
+      </div>
       <h1
         style={inter.style}
-        className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl"
+        className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl py-2"
       >
-        Mint a Token
+        Cogito
       </h1>
 
       <div style={inter.style} className="my-4 text-center">
-        Using Lucid, we can mint a token on the Cardano blockchain.
+        A decentralized and AI-driven stable asset.
       </div>
 
-      <div className="text-left my-8">
-        <div className="my-4">
-          <label className="flex flex-col w-100">
-            <span className="text-sm lowercase mb-1">NFT name</span>
+      {/* <div className="text-left my-8"> */}
+      {/* className="relative flex justify-center items-center w-full h-screen bg-slate-900 text-white flex-col" */}
 
-            <input
-              className="rounded py-1 px-2 text-gray-800 border"
-              name="message"
-              placeholder="My NFT name"
-              value={name || ""}
-              onChange={(e) => setName(e.target.value?.toString())}
-            />
-          </label>
-        </div>
+      <div className="flex flex-row px-2.5 md:flex-row  justify-center w-full justify-around gap-4 place-content-center justify-center ">
+        <div className="w-[500px] max-w-[90%] h-[550px] mt-10 shadow-sm bg-slate-800 rounded-xl flex items-center flex-col p-8 ">
+          <div className="rounded-xl text-xl font-bold mb-6 text-white">
+            Mint Gcoin token
+          </div>
 
-        <div className="my-4">
+          <input
+            className="block w-3/5 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            name="message"
+            placeholder="amount"
+            value={mintAmount || ""}
+            onChange={(e) => setMintAmount(Number(e.target.value))}
+          />
+          {/* </label> */}
+          <div className="border-1 rounded-md ring my-10">
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20  text-white  ">
+              You will pay:{" "}
+              {(mintAmount *
+                (Number(datum.exchangeRate) +
+                  Number(datum.exchangeRate) / 100)) /
+                1000000}
+              ₳
+            </div>
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20 text-white  ">
+              Fees: 1 ₳
+            </div>
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20  text-white  ">
+              Total:{" "}
+              {(mintAmount *
+                (Number(datum.exchangeRate) +
+                  Number(datum.exchangeRate) / 100)) /
+                1000000 +
+                1}{" "}
+              ₳
+            </div>
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20  text-white  ">
+              Exchange Rate ADA:{" "}
+              {(Number(datum.exchangeRate) + Number(datum.exchangeRate) / 100) /
+                1000000}{" "}
+              ₳
+            </div>
+          </div>
           <button
             disabled={!canMint}
-            className="border hover:bg-blue-400 text-white my-4 w-40 py-2 cursor-pointer transition-colors disabled:cursor-not-allowed disabled:text-gray-200 rounded bg-blue-300 disabled:bg-blue-200 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase"
+            className="border hover:bg-blue-800 text-white my-4 w-64 py-2 cursor-pointer transition-colors disabled:cursor-not-allowed disabled:text-gray-200 rounded bg-blue-300 disabled:bg-blue-600 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase"
             onClick={() => {
-              hideToaster()
-              mintNFT()
+              hideToaster();
+              mintNFT();
             }}
           >
             mint
           </button>
+        </div>
 
+        <div className="w-[500px] max-w-[90%] h-[550px] mt-10 shadow-sm bg-slate-800 rounded-xl flex items-center flex-col p-6">
+          <div className="rounded-xl text-xl font-bold mb-6 text-white">
+            Burn Gcoin token
+          </div>
+          <input
+            className="block w-3/5 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            name="message"
+            placeholder="amount"
+            value={burnAmount || ""}
+            onChange={(e) => setBurnAmount(Number(e.target.value))}
+          />
+
+          <div className="border-1 rounded-md ring my-10">
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20  text-white  ">
+              You will get:{" "}
+              {(burnAmount *
+                (Number(datum.exchangeRate) -
+                  Number(datum.exchangeRate) / 100)) /
+                1000000}
+              ₳
+            </div>
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20 text-white  ">
+              Fees: 1 ₳
+            </div>
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20  text-white  ">
+              Total:{" "}
+              {(burnAmount *
+                (Number(datum.exchangeRate) -
+                  Number(datum.exchangeRate) / 100)) /
+                1000000 -
+                1}{" "}
+              ₳
+            </div>
+            <div className="block w-4/5  py-2.5 pl-7  px-2 pr-20  text-white  ">
+              Exchange Rate ADA:{" "}
+              {(Number(datum.exchangeRate) - Number(datum.exchangeRate) / 100) /
+                1000000}{" "}
+              ₳
+            </div>
+          </div>
           <button
-            disabled={!canMint}
-            className="border ml-4 hover:bg-blue-400 text-white w-40 py-2 cursor-pointer transition-colors disabled:cursor-not-allowed disabled:text-gray-200 rounded bg-blue-300 disabled:bg-blue-200 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase"
+            disabled={!canBurn}
+            className="border hover:bg-blue-400 text-white my-4 w-64 py-2 cursor-pointer transition-colors disabled:cursor-not-allowed disabled:text-gray-200 rounded bg-red-300 disabled:bg-red-600 dark:bg-white dark:text-gray-800 dark:disabled:bg-white dark:hover:bg-white font-bold uppercase"
             onClick={() => {
-              hideToaster()
-              burnNFT()
+              hideToaster();
+              burnNFT();
             }}
           >
             burn
@@ -92,5 +187,12 @@ export default function Mint() {
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+
+
+
+
+
+
