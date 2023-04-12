d7d1b0819feaefcba6c586a4b6fcb4c857f6fa9e
eyasubirhanu <eyasubirhanu0@gmail.com>
2023-04-12 16:57:54 +0300

    edit network preview to preprod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,24 @@
 
-import "../styles/globals.css"
-import "use-cardano/styles/use-cardano.css"
-import "../styles/use-cardano-overrides.css"
+import "../styles/globals.css";
+import "use-cardano/styles/use-cardano.css";
+import "../styles/use-cardano-overrides.css";
 
-import type { AppProps } from "next/app"
-import { CardanoProvider, CardanoToaster, UseCardanoOptions } from "use-cardano"
-import { DefaultLayout } from "layouts/DefaultLayout"
+import type { AppProps } from "next/app";
+import {
+  CardanoProvider,
+  CardanoToaster,
+  UseCardanoOptions,
+} from "use-cardano";
+import { DefaultLayout } from "layouts/DefaultLayout";
 
 const options: UseCardanoOptions = {
   allowedNetworks: ["Testnet"],
-  testnetNetwork: "Preview",
+  testnetNetwork: "Preprod",
   node: {
     provider: "blockfrost-proxy",
     proxyUrl: "/api/blockfrost",
   },
-}
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -25,5 +29,5 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <CardanoToaster />
     </CardanoProvider>
-  )
+  );
 }
